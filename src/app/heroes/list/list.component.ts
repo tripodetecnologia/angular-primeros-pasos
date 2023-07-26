d4c211b8a737d2de2f48465ec1bbba54e59e473c
RDSH <desarrollador0992@hotmail.com>
2023-07-25 19:28:24 -0500

    primer commit

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  public heroNames: string[] = ['Spiderman', 'IronMan', 'Hulk', 'Thor', 'She Hulk'];
+  public deletedHero?: string;
+
+  removeLastHero():void
+  {
+      this.deletedHero = this.heroNames.pop();
+  }
+
+}
